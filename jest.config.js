6c9e26a574ea594447a78b1d2db0b521e33afd99
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-01-07 20:48:14 +0200

    chore: Remove `Jest` config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	moduleNameMapper: {
-		"decky-frontend-lib": "decky-frontend-lib",
-	},
-	transformIgnorePatterns: ["/node_modules/(?!decky-frontend-lib/)"],
-};
