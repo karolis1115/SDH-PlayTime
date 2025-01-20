cdc419bc261d6983160e1870ac857e53c18a4db5
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-01-20 20:34:37 +0200

    feat(components): Add new properties for `FocusableExt`

--- src/components/FocusableExt.tsx
+++ src/components/FocusableExt.tsx
@@ -2,14 +2,28 @@ import { Focusable } from "@decky/ui";
 import { focus_panel_no_padding } from "../styles";
 
 interface FocusableExtProperties {
-	children: JSX.Element;
+	children: JSX.Element | Array<JSX.Element>;
+	focusWithinClassName?: string;
+	onActivate?: () => void;
+	onOptionsActionDescription?: JSX.Element;
+	onOptionsButton?: () => void;
 }
 
 export const FocusableExt: React.FC<FocusableExtProperties> = ({
 	children,
+	focusWithinClassName,
+	onActivate = () => {},
+	onOptionsActionDescription,
+	onOptionsButton,
 }) => {
 	return (
-		<Focusable style={focus_panel_no_padding} onActivate={() => {}}>
+		<Focusable
+			focusWithinClassName={focusWithinClassName}
+			onActivate={onActivate}
+			onOptionsActionDescription={onOptionsActionDescription}
+			onOptionsButton={onOptionsButton}
+			style={focus_panel_no_padding}
+		>
 			{children}
 		</Focusable>
 	);
