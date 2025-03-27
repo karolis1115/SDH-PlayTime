3e1730b20e2cff6d8769a817ed5034d4c57edd9b
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-03-27 19:06:32 +0200

    refactor(SettingsPage): Remove unnecessary render condition

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -12,6 +12,10 @@ import { Tab } from "../components/Tab";
 import { useLocator } from "../locator";
 import { MANUALLY_ADJUST_TIME, navigateToPage } from "./navigation";
 
+const SCALE_OPTIONS = [
+	0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2,
+];
+
 export const GeneralSettings = () => {
 	const { settings } = useLocator();
 	const [current, setCurrent] = useState<PlayTimeSettings>(DEFAULTS);
@@ -32,117 +36,115 @@ export const GeneralSettings = () => {
 
 	const updateSettings = async () => {
 		await settings.save(current);
+
 		loadSettings();
 	};
 
+	if (!loaded) {
+		return null;
+	}
+
 	return (
-		<div>
-			{loaded && (
-				<div>
-					<PanelSection title="Appearance">
-						<PanelSectionRow>
-							<Field label="Game charts type">
-								<Dropdown
-									selectedOption={current?.gameChartStyle}
-									rgOptions={[
-										{
-											label: "Bar charts",
-											data: ChartStyle.BAR,
-										},
-										{
-											label: "Bar and Pie charts",
-											data: ChartStyle.PIE_AND_BARS,
-										},
-									]}
-									onChange={(v) => {
-										current.gameChartStyle = v.data;
-										updateSettings();
-									}}
-								/>
-							</Field>
+		<>
+			<PanelSection title="Appearance">
+				<PanelSectionRow>
+					<Field label="Game charts type">
+						<Dropdown
+							selectedOption={current?.gameChartStyle}
+							rgOptions={[
+								{
+									label: "Bar charts",
+									data: ChartStyle.BAR,
+								},
+								{
+									label: "Bar and Pie charts",
+									data: ChartStyle.PIE_AND_BARS,
+								},
+							]}
+							onChange={(v) => {
+								current.gameChartStyle = v.data;
+								updateSettings();
+							}}
+						/>
+					</Field>
 
-							<Field label="Display played time in">
-								<Dropdown
-									selectedOption={current?.displayTime.showTimeInHours}
-									rgOptions={[
-										{
-											label: "Days",
-											data: false,
-										},
-										{
-											label: "Hours",
-											data: true,
-										},
-									]}
-									onChange={(v) => {
-										current.displayTime.showTimeInHours = v.data;
-										updateSettings();
-									}}
-								/>
-							</Field>
+					<Field label="Display played time in">
+						<Dropdown
+							selectedOption={current?.displayTime.showTimeInHours}
+							rgOptions={[
+								{
+									label: "Days",
+									data: false,
+								},
+								{
+									label: "Hours",
+									data: true,
+								},
+							]}
+							onChange={(v) => {
+								current.displayTime.showTimeInHours = v.data;
+								updateSettings();
+							}}
+						/>
+					</Field>
 
-							<Field label="Show seconds">
-								<Dropdown
-									selectedOption={current?.displayTime.showSeconds}
-									rgOptions={[
-										{
-											label: "No",
-											data: false,
-										},
-										{
-											label: "Yes",
-											data: true,
-										},
-									]}
-									onChange={(v) => {
-										current.displayTime.showSeconds = v.data;
-										updateSettings();
-									}}
-								/>
-							</Field>
+					<Field label="Show seconds">
+						<Dropdown
+							selectedOption={current?.displayTime.showSeconds}
+							rgOptions={[
+								{
+									label: "No",
+									data: false,
+								},
+								{
+									label: "Yes",
+									data: true,
+								},
+							]}
+							onChange={(v) => {
+								current.displayTime.showSeconds = v.data;
+								updateSettings();
+							}}
+						/>
+					</Field>
 
-							<Field label="Scale covers size">
-								<Dropdown
-									selectedOption={+current?.coverScale.toPrecision(2)}
-									rgOptions={[
-										0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6,
-										1.7, 1.8, 1.9, 2,
-									].map((scale) => ({
-										label: `${scale}`,
-										data: scale,
-									}))}
-									onChange={(v) => {
-										current.coverScale = v.data;
-										updateSettings();
-									}}
-								/>
-							</Field>
-						</PanelSectionRow>
-					</PanelSection>
+					<Field label="Scale covers size">
+						<Dropdown
+							selectedOption={+current?.coverScale.toPrecision(2)}
+							rgOptions={SCALE_OPTIONS.map((scale) => ({
+								label: `${scale}`,
+								data: scale,
+							}))}
+							onChange={(v) => {
+								current.coverScale = v.data;
+								updateSettings();
+							}}
+						/>
+					</Field>
+				</PanelSectionRow>
+			</PanelSection>
 
-					<PanelSection title="Notifications">
-						<PanelSectionRow>
-							<Field label="Remind me to take breaks">
-								<Dropdown
-									selectedOption={current.reminderToTakeBreaksInterval}
-									rgOptions={[
-										{ label: "Never", data: -1 },
-										{ label: "Every 15 min", data: 15 },
-										{ label: "Every 30 min", data: 30 },
-										{ label: "Every hour", data: 60 },
-										{ label: "Every 2 hours", data: 120 },
-									]}
-									onChange={(v) => {
-										current.reminderToTakeBreaksInterval = v.data;
-										updateSettings();
-									}}
-								/>
-							</Field>
-						</PanelSectionRow>
-					</PanelSection>
-				</div>
-			)}
-		</div>
+			<PanelSection title="Notifications">
+				<PanelSectionRow>
+					<Field label="Remind me to take breaks">
+						<Dropdown
+							selectedOption={current.reminderToTakeBreaksInterval}
+							rgOptions={[
+								{ label: "Never", data: -1 },
+								{ label: "Every 15 min", data: 15 },
+								{ label: "Every 30 min", data: 30 },
+								{ label: "Every hour", data: 60 },
+								{ label: "Every 2 hours", data: 120 },
+							]}
+							onChange={(v) => {
+								current.reminderToTakeBreaksInterval = v.data;
+								updateSettings();
+							}}
+						/>
+					</Field>
+				</PanelSectionRow>
+			</PanelSection>
+		</>
 	);
 };
 
