ba0812a29630c4ff7281d20c0d67315baf16674b
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-04-05 12:41:32 +0300

    feat(files): Improve file hashing
    - Remove methods related to base64

--- python/files.py
+++ python/files.py
@@ -1,41 +1,45 @@
 # https://www.geeksforgeeks.org/sha-in-python/
 import hashlib
-
-import base64
 import os
 import sys
 
 data_dir = os.environ["DECKY_PLUGIN_RUNTIME_DIR"]
 
+# 16MB
+CHUNK_SIZE = 16 * 1024 * 1024
+
 
 class Files:
-    def get_file_base64(self, path: str):
-        with open(path, "rb") as image_file:
-            base64_bytes = base64.b64encode(image_file.read())
-
-            return base64_bytes.decode()
-
-    def get_game_cover_base64_by_id(self, game_id: str):
-        if os.path.exists(f"{data_dir}/covers/{game_id}.png"):
-            return self.get_file_base64(f"{data_dir}/covers/{game_id}.png")
-
-        if os.path.exists(f"{data_dir}/covers/{game_id}.jpg"):
-            return self.get_file_base64(f"{data_dir}/covers/{game_id}.jpg")
-
-        raise Exception("Can not find cover image for gameId: ", game_id)
-
     # NOTE(ynhhoJ): https://stackoverflow.com/a/44873382
-    def get_file_sha256(self, path: str):
+    def get_file_sha256(self, file_path: str, chunk_size: int = CHUNK_SIZE) -> str:
+        """
+        Compute a SHA256 hash of the first and last `chunk` of a file.
+        If the file is smaller than `chunk_size`, hash the entire file.
+
+        Args:
+            `file_path` (str): Path to the file.
+
+        Returns:
+            str: `sha256` hex digest of the file.
+        """
+
         if sys.version_info < (3, 11):
-            raise Exception(
+            raise RuntimeError(
                 "Minimum required version of Python is 3.11.0 which supports: hashlib.file_digest"
             )
 
-        if not os.path.exists(f"{data_dir}/{path}"):
-            raise Exception(
-                "Can not find file at path: ",
-                f"{data_dir}/{path}",
-            )
+        file_size = os.path.getsize(file_path)
+        hasher = hashlib.sha256()
 
-        with open(f"{data_dir}/{path}", "rb", buffering=0) as f:
-            return hashlib.file_digest(f, "sha256").hexdigest()
+        with open(file_path, "rb") as f:
+            if file_size <= chunk_size * 2:
+                return hashlib.file_digest(f, "sha256").hexdigest()
+
+            first_chunk = f.read(chunk_size)
+            hasher.update(memoryview(first_chunk))
+
+            f.seek(-chunk_size, os.SEEK_END)
+            last_chunk = f.read(chunk_size)
+            hasher.update(memoryview(last_chunk))
+
+        return hasher.hexdigest()
