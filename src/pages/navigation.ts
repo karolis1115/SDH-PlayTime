2f0b9d02bbf56d65adb43f22490e75165dc894f8
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-05-13 13:57:07 +0300

    feat(navigation): Add files checksum route

--- src/pages/navigation.ts
+++ src/pages/navigation.ts
@@ -3,6 +3,7 @@ import { Navigation } from "@decky/ui";
 export const DETAILED_REPORT_ROUTE = "/playtime/detailed-report";
 export const GAME_REPORT_ROUTE = "/playtime/game-report-route/:gameId";
 export const MANUALLY_ADJUST_TIME = "/playtime/manually-adjust-time";
+export const FILE_CHECKSUM_ROUTE = "/playtime/file-checksum";
 export const SETTINGS_ROUTE = "/playtime/settings";
 
 export function navigateToPage(url: string) {
