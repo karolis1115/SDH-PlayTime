d3c741d5626c0a6cd917b5b908599da28e2e66d2
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-05-13 13:56:34 +0300

    test(back-end/files): Add tests for `SHA256`

--- python/tests/files_test.py
+++ python/tests/files_test.py
@@ -1,31 +1,35 @@
+import os
+import tempfile
+import time
 import unittest
 from unittest.mock import patch
 
 with patch.dict("os.environ", {"DECKY_PLUGIN_RUNTIME_DIR": "./python/tests/data"}):
     from python.files import Files
 
+CHUNK_SIZE = 16 * 1024 * 1024
+
 
 class TestFiles(unittest.TestCase):
     files: Files = Files()
 
     def test_get_file_sha256(self):
-        result = self.files.get_file_sha256("/covers/0.jpg")
+        start_time = time.perf_counter()  # Start timing
+        # result = self.files.get_file_sha256("./python/tests/data/covers/0.jpg")
+        result = self.files.get_file_sha256(
+            "./python/tests/data/covers/Monster Hunter 4 Ultimate.3ds"
+        )
+
+        end_time = time.perf_counter()  # End timing
+        elapsed_time = end_time - start_time  # Compute execution time
+
+        print(f"Execution SHA256 time: {elapsed_time:.6f} seconds")
 
         cover_sha256 = (
             "ef24abaf50ea055af5cc3d45c586b47a111c33869ca70e738ff99463692711cf"
         )
 
         self.assertEqual(result, cover_sha256)
-
-    def test_get_cover_base64(self):
-        result = self.files.get_game_cover_base64_by_id("0")
-
-        self.assertEqual(
-            result.startswith(
-                "/9j/7AARRHVja3kAAQAEAAAAUAAA/+4ADkFkb2JlAGTAAAAAAf/bAIQAAgICAgICAgICAgMCAgIDBAMCAgMEBQQEBAQEBQYFBQUFBQUGBgcHCAcHBgkJCgoJCQwMDAwMDAwMDAwMDAwMDAEDAwMFBAUJBgYJDQsJCw0PDg4ODg8PDAwMDAwPDwwMDAwMDA8MDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwM/8AAEQgDhAJYAwERAAIRAQMRAf/EAPEAAAAGAwEBAAAAAAAAAAAAAAAFBgcICQECBAMKAQABBQEBAQEAAAAAAAAAAAAFAAIDBAYBBwgJEAABAwMDAgQDBQMIBwQBAicBAgMEEQUGACESMQdBIhMIUWEUcYEyIxWRQgmhsdFSYjMkFsFygpJDU1ThojQlF2NEGPDxc9NksoOTo8OU1DW1JhnSlTZ2JzjCs3SEpLRFVWV1ZjdXdygpEQABAwIEAgcGAwUGBAQEBAcBAAIDEQQhMRIFQVFhcYGRIhMG8KGxwdEy4UIUUmJyIwfxgpKiMxWywtJTQ5MkFmM0VAjic4OjRGSz1BdFGP/aAAwDAQACEQMRAD8Ao0+pkf8APc/3j/Tr2+gVRD6mR/z3P94/06VAkh9TI/57n+8f6dKgSWPqZH/Pc/3j/TrhAXVqZMgf8dz"
-            ),
-            True,
-        )
 
 
 if __name__ == "__main__":
