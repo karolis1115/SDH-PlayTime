99321bea544a31b298ca9660d1f19fc80e6dff5e
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-05-15 13:03:43 +0300

    feat(backend/main): Use Decky logger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import decky
 import dataclasses
-import logging
 import os
 import sys
 import asyncio
@@ -13,23 +12,16 @@ log_dir = os.environ["DECKY_PLUGIN_LOG_DIR"]
 data_dir = os.environ["DECKY_PLUGIN_RUNTIME_DIR"]
 plugin_dir = Path(os.environ["DECKY_PLUGIN_DIR"])
 
-logging.basicConfig(
-    filename=f"{log_dir}/decky-playtime.log",
-    format="[Playtime] %(asctime)s %(levelname)s %(message)s",
-    filemode="w+",
-    force=True,
-)
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
 
 def add_plugin_to_path():
     directories = [["./"], ["python"]]
+
     for import_dir in directories:
         sys.path.append(str(plugin_dir.joinpath(*import_dir)))
 
 
 add_plugin_to_path()
+
 # pylint: disable=wrong-import-position
 from python.db.dao import Dao
 from python.db.migration import DbMigration
@@ -62,7 +54,7 @@ class Plugin:
             self.statistics = Statistics(dao)
             self.time_tracking = TimeTracking(dao)
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception("[_main] Unhandled exception: ", str(e))
 
     async def add_time(
         self, started_at: int, ended_at: int, game_id: str, game_name: str
@@ -75,7 +67,7 @@ class Plugin:
                 game_name=game_name,
             )
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception("[add_time] Unhandled exception: ", str(e))
 
     async def daily_statistics_for_period(
         self, start_date: str, end_date: str, game_id: str
@@ -87,13 +79,17 @@ class Plugin:
                 )
             )
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception(
+                "[daily_statistics_for_period] Unhandled exception: ", str(e)
+            )
 
     async def per_game_overall_statistics(self):
         try:
             return self.statistics.per_game_overall_statistic()
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception(
+                "[per_game_overall_statistics] Unhandled exception: ", str(e)
+            )
 
     async def apply_manual_time_correction(
         self, list_of_game_stats: List[dict[str, Any]]
@@ -103,13 +99,15 @@ class Plugin:
                 list_of_game_stats=list_of_game_stats, source="manually-changed"
             )
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception(
+                "[apply_manual_time_correction] Unhandled exception: ", str(e)
+            )
 
     async def get_game(self, game_id: str):
         try:
             return dataclasses.asdict(self.games.get_by_id(game_id))
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception("[get_game] Unhandled exception: ", str(e))
 
     async def has_min_required_python_version(self) -> bool:
         if sys.version_info < (3, 15):
@@ -121,20 +119,22 @@ class Plugin:
         try:
             return await asyncio.to_thread(self.files.get_file_sha256, path)
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception("[get_file_sha256] Unhandled exception: ", str(e))
 
     async def get_games_dictionary(self):
         try:
             return self.games.get_dictionary()
         except Exception as e:
-            logger.exception("Unhandled exception: ", str(e))
+            decky.logger.exception(
+                "[get_games_dictionary] Unhandled exception: ", str(e)
+            )
 
     async def _unload(self):
-        logger.info("Goodnight, World!")
+        decky.logger.info("Goodnight, World!")
 
         pass
 
     async def _uninstall(self):
-        logger.info("Goodbye, World!")
+        decky.logger.info("Goodbye, World!")
 
         pass
