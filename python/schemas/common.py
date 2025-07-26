5f82fe20c15b964b9d93733a8ca4e5e87d7d4bcb
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-07-26 19:43:15 +0300

    feat(backend/schemas): Add `common` types

---
+++ python/schemas/common.py
@@ -0,0 +1,12 @@
+from typing import Literal
+
+type Checksum = Literal[
+    "SHA224",
+    "SHA256",
+    "SHA384",
+    "SHA512",
+    "SHA3_224",
+    "SHA3_256",
+    "SHA3_384",
+    "SHA3_512",
+]
